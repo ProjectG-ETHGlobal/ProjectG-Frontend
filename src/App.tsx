7eba7e6137ca4d526ebb55e4c3b065446f742348
Pranav M S <pranavms13@yahoo.com>
2024-09-22 01:33:42 +0800

    Added Wallet Auth and basic API Service

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,19 @@ import AuthenticationScreen from "./routes/AuthenticationScreen";
 import DeliveryPartnerScreen from "./routes/DeliveryPartnerScreen";
 import ProductsScreen from "./routes/ProductsScreen";
 import SupplierScreen from "./routes/SupplierScreen";
+import { Web3ModalProvider } from "./providers/WalletProvider";
 
 export default function App() {
 	return (
-		<Router>
-			<Routes>
-				<Route path="/" element={<AuthenticationScreen />} />
-				<Route path="/user" element={<ProductsScreen />} />
-				<Route path="/supplier" element={<SupplierScreen />} />
-				<Route path="/deliveryPartner" element={<DeliveryPartnerScreen />} />
-			</Routes>
-		</Router>
+		<Web3ModalProvider>
+			<Router>
+				<Routes>
+					<Route path="/" element={<AuthenticationScreen />} />
+					<Route path="/user" element={<ProductsScreen />} />
+					<Route path="/supplier" element={<SupplierScreen />} />
+					<Route path="/deliveryPartner" element={<DeliveryPartnerScreen />} />
+				</Routes>
+			</Router>
+		</Web3ModalProvider>
 	);
 }
